d95fdb60e847be769f46f85165db2c52397ceb72
Orin Fletcher <orin.fletcher@gmail.com>
2017-10-12 22:42:04 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -83,5 +83,7 @@ function removeFromCart(item){
 //console.log(removeFromCart("piebald"))
 
 function placeOrder(cardNumber) {
-  
+  if(!cardNumber){
+    console.log("Sorry, we don't have a credit card on file for you.")
+  }
 }
