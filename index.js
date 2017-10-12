40b3aa043168cd22332a3e2999d1e49870285ccc
Orin Fletcher <orin.fletcher@gmail.com>
2017-10-12 21:15:06 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -27,9 +27,7 @@ function viewCart() {
   let itemsCosts = [] //new array for items + costs
   for (let i = 0; i < cart.length; i++) {
     let cartItem = cart[i] //on each iteration of the loop, the cart[index] is assigned to itemsCosts
-    console.log(cartItem)
     let cartItemKey = Object.keys(cartItem)[0] //cartItem stores the key of the object at cart[i][0], etc...
-    console.log(cartItemKey)
     let cartItemValue = cartItem[cartItemKey] //assigns value to key
     console.log('cartItem = ${cartItem} /n cartItemKey = ${cartItemKey} /n cartItemValue = ${cartItemValue}')
     itemsCosts.push(`${cartItem} at $${cartItemValue}`)
