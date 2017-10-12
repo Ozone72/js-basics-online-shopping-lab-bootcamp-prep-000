6a55e9cf00510f6b1cc18586688138c72b5de91f
Orin Fletcher <orin.fletcher@gmail.com>
2017-10-12 21:41:41 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -50,7 +50,13 @@ viewCart();
 console.log(cart)
 
 function total() {
-
+  let sum = 0
+  for (let i = 0; i < array.length; i++) {
+    for (let item in cart) {
+      sum+=cart[i][item]
+    }
+  }
+  return sum
 }
 
 function removeFromCart(item) {
